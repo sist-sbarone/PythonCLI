703f31085ba017af0a4c4fe6712f4333714b7db7
Stefano Barone <stefano.barone@grupposistematica.it>
2025-05-07 17:41:57 +0200

    configured entrypoint for application

--- src/klickbrick/klickbrick.py
+++ src/klickbrick/klickbrick.py
@@ -3,18 +3,23 @@
 
 import argparse
 
-
-
-if __name__ == "__main__":
+def start():
     parser = argparse.ArgumentParser(description="This script executes a Hello World",  # main description for help
             epilog='Usage samples : \n\tpython klickbrick.py hello \n\n', formatter_class=argparse.RawTextHelpFormatter)  # displayed after help
     subparsers = parser.add_subparsers(
     title="subcommands", help="hello commands")
     hello_parser = subparsers.add_parser("hello", help="hello message")
-#    parser.add_argument("hello",
-#                        help="Main argument")
+    parser.add_argument("-n", "--name",
+                        help="Your name ",
+                        required=False)
+
 
 
     args = parser.parse_args()
+    
+    target = args.name if args.name else 'World'
+    print("Hello ", target)
+    exit(0)
 
-    print("Hello World")
+if __name__ == "__main__":
+    start()
