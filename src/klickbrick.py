b417862561fea2883e817001a023d90d48a3995e
Stefano Barone <stefano.barone@grupposistematica.it>
2025-05-07 16:00:24 +0200

    Defined command hello

--- src/klickbrick.py
+++ src/klickbrick.py
@@ -8,6 +8,13 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="This script executes a Hello World",  # main description for help
             epilog='Usage samples : \n\tpython klickbrick.py hello \n\n', formatter_class=argparse.RawTextHelpFormatter)  # displayed after help
-#    parser.add_argument("-k", "--key",
-#                        help="Mars ADS key",
-#                        required=True)
+    subparsers = parser.add_subparsers(
+    title="subcommands", help="hello commands")
+    hello_parser = subparsers.add_parser("hello", help="hello message")
+#    parser.add_argument("hello",
+#                        help="Main argument")
+
+
+    args = parser.parse_args()
+
+    print("Hello World")
